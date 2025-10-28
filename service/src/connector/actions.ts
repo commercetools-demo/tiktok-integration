543ffab0db3f3fd5eb7b0d1bcd24e3a0a68920d1
Behnam Tehrani <behnam777@gmail.com>
2025-10-28 10:43:00 -0400

    remove predeploy

--- service/src/connector/actions.ts
+++ service/src/connector/actions.ts
@@ -52,33 +52,6 @@ export async function createCartUpdateExtension(
     .execute();
 }
 
-export async function deleteCartUpdateExtension(apiRoot: ByProjectKeyRequestBuilder): Promise<void> {
-  const {
-    body: { results: extensions },
-  } = await apiRoot
-    .extensions()
-    .get({
-      queryArgs: {
-        where: `key = "${CART_UPDATE_EXTENSION_KEY}"`,
-      },
-    })
-    .execute();
-
-  if (extensions.length > 0) {
-    const extension = extensions[0];
-
-    await apiRoot
-      .extensions()
-      .withKey({ key: CART_UPDATE_EXTENSION_KEY })
-      .delete({
-        queryArgs: {
-          version: extension.version,
-        },
-      })
-      .execute();
-  }
-}
-
 export async function createCustomCartDiscountType(apiRoot: ByProjectKeyRequestBuilder): Promise<void> {
   const {
     body: { results: types },
